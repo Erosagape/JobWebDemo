392ccdbd71792a820d8b10ecd7c6167e61bbc85c
Puttipong <leoputti@hotmail.com>
2018-05-02 18:00:31 +0700

    Class Database Generator

---
+++ GenClassFromDB/Form1.Designer.vb
@@ -0,0 +1,173 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.TextBox2 = New System.Windows.Forms.TextBox()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TextBox4 = New System.Windows.Forms.TextBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(19, 8)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(691, 20)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Enabled = False
+        Me.TextBox2.Location = New System.Drawing.Point(19, 34)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(691, 114)
+        Me.TextBox2.TabIndex = 1
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(19, 155)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(397, 258)
+        Me.DataGridView1.TabIndex = 2
+        '
+        'TextBox3
+        '
+        Me.TextBox3.Location = New System.Drawing.Point(422, 178)
+        Me.TextBox3.Multiline = True
+        Me.TextBox3.Name = "TextBox3"
+        Me.TextBox3.Size = New System.Drawing.Size(366, 235)
+        Me.TextBox3.TabIndex = 3
+        '
+        'Button1
+        '
+        Me.Button1.Enabled = False
+        Me.Button1.Location = New System.Drawing.Point(717, 112)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(71, 37)
+        Me.Button1.TabIndex = 4
+        Me.Button1.Text = "OK"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Location = New System.Drawing.Point(717, 35)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton1.TabIndex = 5
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "VB"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Location = New System.Drawing.Point(717, 58)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(39, 17)
+        Me.RadioButton2.TabIndex = 6
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "C#"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Items.AddRange(New Object() {"OleDb", "Sql", "Oracle", "MySql", "Odbc"})
+        Me.ComboBox1.Location = New System.Drawing.Point(717, 82)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(71, 21)
+        Me.ComboBox1.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(717, 8)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "TEST"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(423, 155)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(63, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Class Name"
+        '
+        'TextBox4
+        '
+        Me.TextBox4.Location = New System.Drawing.Point(492, 152)
+        Me.TextBox4.Name = "TextBox4"
+        Me.TextBox4.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox4.TabIndex = 10
+        Me.TextBox4.Text = "CTableName"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.RadioButton2)
+        Me.Controls.Add(Me.RadioButton1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox3)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox4 As TextBox
+End Class
---
+++ GenClassFromDB/Form1.vb
@@ -0,0 +1,143 @@
+﻿Imports System.Data.OleDb
+Imports System.Data
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = My.Settings.ConnectionStr
+        TextBox2.Text = My.Settings.DefaultSQL
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim bConnect As Boolean
+        Try
+            Dim oConn As New OleDbConnection(TextBox1.Text)
+            oConn.Open()
+            bConnect = True
+            MessageBox.Show("Connected")
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+        TextBox2.Enabled = bConnect
+        Button1.Enabled = bConnect
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            Dim dt As New DataTable
+            Dim da As New OleDbDataAdapter(TextBox2.Text, TextBox1.Text)
+            da.Fill(dt)
+            DataGridView1.DataSource = dt
+            TextBox3.Text = ReadStructure(dt)
+        Catch ex As Exception
+            MessageBox.Show(ex.Message)
+        End Try
+    End Sub
+    Private Function ReadStructure(oTbl As DataTable) As String
+        Dim strAll As String = ""
+        strAll &= "Imports System.Data" & vbCrLf
+        strAll &= "Imports System.Data.OleDb" & vbCrLf
+        strAll &= "Public Class " & TextBox4.Text & " " & vbCrLf
+        strAll &= "Private m_ConnStr as String" & vbCrLf
+        strAll &= "Public Sub New(pConnStr as String)" & vbCrLf
+        strAll &= "m_ConnStr=pConnStr" & vbCrLf
+        strAll &= "End sub" & vbCrLf
+        Dim strPrivate As String = ""
+        Dim strProperty As String = ""
+        Dim strReader As String = ""
+        Dim strWriter As String = ""
+        For Each dc As DataColumn In oTbl.Columns
+            strProperty = ""
+            If strPrivate <> "" Then strPrivate &= vbCrLf
+            If strReader <> "" Then strReader &= vbCrLf
+            If strWriter <> "" Then strWriter &= vbCrLf
+            Select Case Replace(dc.DataType.FullName, "System.", "")
+                Case "String"
+                    strReader &= "row." & dc.ColumnName & " =rd.GetString(rd.GetOrdinal(""" & dc.ColumnName & """))"
+                    strPrivate &= "Private m_" & dc.ColumnName & " as String"
+                    strProperty &= vbCrLf & "Public Property " & dc.ColumnName & " as String"
+                    strProperty &= vbCrLf & "Get"
+                    strProperty &= vbCrLf & "Return m_" & dc.ColumnName
+                    strProperty &= vbCrLf & "End Get"
+                    strProperty &= vbCrLf & "Set (value as String)"
+                    strProperty &= vbCrLf & "m_" & dc.ColumnName & " =value"
+                    strProperty &= vbCrLf & "End Set"
+                Case "Double"
+                    strPrivate &= "Private m_" & dc.ColumnName & " as Double"
+                    strReader &= "row." & dc.ColumnName & " =rd.GetDouble(rd.GetOrdinal(""" & dc.ColumnName & """))"
+                    strProperty &= vbCrLf & "Public Property " & dc.ColumnName & " as Double"
+                    strProperty &= vbCrLf & "Get"
+                    strProperty &= vbCrLf & "Return m_" & dc.ColumnName
+                    strProperty &= vbCrLf & "End Get"
+                    strProperty &= vbCrLf & "Set (value as Double)"
+                    strProperty &= vbCrLf & "m_" & dc.ColumnName & " =value"
+                    strProperty &= vbCrLf & "End Set"
+                Case "DateTime"
+                    strPrivate &= "Private m_" & dc.ColumnName & " as Date"
+                    strReader &= "row." & dc.ColumnName & " =rd.GetDateTime(rd.GetOrdinal(""" & dc.ColumnName & """))"
+                    strProperty &= vbCrLf & "Public Property " & dc.ColumnName & " as Date"
+                    strProperty &= vbCrLf & "Get"
+                    strProperty &= vbCrLf & "Return m_" & dc.ColumnName
+                    strProperty &= vbCrLf & "End Get"
+                    strProperty &= vbCrLf & "Set (value as Date)"
+                    strProperty &= vbCrLf & "m_" & dc.ColumnName & " =value"
+                    strProperty &= vbCrLf & "End Set"
+                Case Else
+                    strPrivate &= "Private m_" & dc.ColumnName & " as Integer"
+                    strReader &= "row." & dc.ColumnName & " =rd.Get" & Replace(dc.DataType.FullName, "System.", "") & "(rd.GetOrdinal(""" & dc.ColumnName & """))"
+                    strProperty &= vbCrLf & "Public Property " & dc.ColumnName & " as Integer"
+                    strProperty &= vbCrLf & "Get"
+                    strProperty &= vbCrLf & "Return m_" & dc.ColumnName
+                    strProperty &= vbCrLf & "End Get"
+                    strProperty &= vbCrLf & "Set (value as Integer)"
+                    strProperty &= vbCrLf & "m_" & dc.ColumnName & " =value"
+                    strProperty &= vbCrLf & "End Set"
+            End Select
+            strWriter &= "dr(""" & dc.ColumnName & """)=me." & dc.ColumnName & ""
+            strProperty &= vbCrLf & "End Property"
+            strPrivate &= strProperty
+        Next
+        strAll = strAll & vbCrLf & strPrivate
+        strAll = strAll & vbCrLf & "Public Function SaveData(pSQLWhere as string) as Boolean"
+        strAll = strAll & vbCrLf & "dim bComplete as boolean=false"
+        strAll = strAll & vbCrLf & "using cn as new OledbConnection(m_ConnStr)"
+        strAll = strAll & vbCrLf & "try"
+        strAll = strAll & vbCrLf & "cn.Open"
+        strAll = strAll & vbCrLf & "using da as new OledbDataAdapter(""" & TextBox2.Text & """ & pSQLWhere,cn)"
+        strAll = strAll & vbCrLf & "using cb as new OledbCommandBuilder(da)"
+        strAll = strAll & vbCrLf & "using dt as new DataTable"
+        strAll = strAll & vbCrLf & "da.fill(dt)"
+        strAll = strAll & vbCrLf & "dim dr as DataRow=dt.NewRow"
+        strAll = strAll & vbCrLf & "if dt.Rows.Count>0 then dr=dt.rows(0)"
+        strAll = strAll & vbCrLf & strWriter
+        strAll = strAll & vbCrLf & "if dr.RowState=DataRowState.Detached then dt.rows.add(dr)"
+        strAll = strAll & vbCrLf & "da.update(dt)"
+        strAll = strAll & vbCrLf & "bComplete=True"
+        strAll = strAll & vbCrLf & "End Using"
+        strAll = strAll & vbCrLf & "End Using"
+        strAll = strAll & vbCrLf & "End Using"
+        strAll = strAll & vbCrLf & "catch ex as Exception"
+        strAll = strAll & vbCrLf & "End try"
+        strAll = strAll & vbCrLf & "End Using"
+        strAll = strAll & vbCrLf & "Return bComplete"
+        strAll = strAll & vbCrLf & "End Function"
+
+
+        strAll = strAll & vbCrLf & "Public Function GetData(pSQLWhere as string) as List(Of " & TextBox4.Text & " ) "
+        strAll = strAll & vbCrLf & "Dim lst as new List(Of " & TextBox4.Text & " )"
+        strAll = strAll & vbCrLf & "using cn as new OledbConnection(m_ConnStr)"
+        strAll = strAll & vbCrLf & "Dim row as " & TextBox4.Text & " "
+        strAll = strAll & vbCrLf & "try"
+        strAll = strAll & vbCrLf & "cn.Open"
+        strAll = strAll & vbCrLf & "Dim rd as OledbDataReader=new OledbCommand(""" & TextBox2.Text & """ & pSQLWhere,cn).ExecuteReader()"
+        strAll = strAll & vbCrLf & "while rd.Read()"
+        strAll = strAll & vbCrLf & "row=New " & TextBox4.Text & " (m_ConnStr)"
+        strAll = strAll & vbCrLf & strReader
+        strAll = strAll & vbCrLf & "lst.Add(row)"
+        strAll = strAll & vbCrLf & "End While"
+        strAll = strAll & vbCrLf & "catch ex as Exception"
+        strAll = strAll & vbCrLf & "End try"
+        strAll = strAll & vbCrLf & "End Using"
+        strAll = strAll & vbCrLf & "Return lst"
+        strAll = strAll & vbCrLf & "End Function"
+        Return strAll & vbCrLf & "End Class"
+    End Function
+End Class
